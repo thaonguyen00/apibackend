839e2d3ee8ddef14e9d058f95889d646491b4d9f
Thao Nguyen <thao.nguyen@zaplabs.com>
2019-03-27 13:19:55 -0700

    adds gRPC

---
+++ gRPC/interceptors.go
@@ -0,0 +1,36 @@
+package gRPC
+
+import (
+	"github.com/davecgh/go-spew/spew"
+	"go.opencensus.io/trace"
+	"google.golang.org/grpc"
+	"os"
+	"time"
+	"golang.org/x/net/context"
+	"fmt"
+	klog "github.com/go-kit/kit/log"
+)
+
+func GrpcTracer(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+	ctx, span := trace.StartSpan(ctx, "Endpoint Request")
+	defer span.End()
+	span.Annotate([]trace.Attribute{
+		trace.StringAttribute("request", fmt.Sprintf("%v", req)),
+	}, "Input")
+
+	return handler(ctx, req)
+}
+
+func GrpcLogger(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+	logger := klog.NewLogfmtLogger(os.Stderr)
+	defer func(begin time.Time) {
+		_ = logger.Log(
+			"method", "Endpoint Request",
+			"err", err,
+			"request", spew.Sdump(req),
+			"took", time.Since(begin),
+		)
+	}(time.Now())
+
+	return handler(ctx, req)
+}
