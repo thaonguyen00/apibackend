e155bf8a59d1305f8adc66d4de54207a95ced3f8
Thao Nguyen <thao.nguyen@zaplabs.com>
2019-05-14 19:45:14 -0700

    adds authorization

---
+++ authorization/auth.go
@@ -0,0 +1,109 @@
+package authorization
+
+import (
+	"context"
+	"encoding/base64"
+	"github.com/pkg/errors"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+
+
+func basicAuth(username, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
+}
+
+func redirectPolicyFunc(req *http.Request, via []*http.Request) error{
+	req.Header.Add("Authorization","Basic " + basicAuth("username1","password123"))
+	return nil
+}
+
+
+func Handler(ctx context.Context, opaServer string, attributes string) (bool, error){
+	url := ConstructURL(opaServer, attributes)
+	user, pass, err := GetAuthFromContext(ctx)
+	if err != nil {
+		return false, err
+	}
+	request, err := Authen("GET", url, user, pass)
+
+
+	return request, err
+}
+
+// Get username, password from grpc context
+func GetAuthFromContext(ctx context.Context) (string, string, error){
+	md, _ := metadata.FromIncomingContext(ctx)
+
+	auth := md.Get("grpcgateway-authorization")
+	const prefix = "Basic "
+
+	if !strings.HasPrefix(auth[0], prefix) {
+		return "","", status.Error(codes.Unauthenticated, `missing "Basic " prefix in "Authorization" header`)
+	}
+
+	c, err := base64.StdEncoding.DecodeString(auth[0][len(prefix):])
+	if err != nil {
+		return  "","", status.Error(codes.Unauthenticated, `invalid base64 in header`)
+	}
+
+	cs := string(c)
+	so := strings.IndexByte(cs, ':')
+	if so < 0 {
+		return  "","", status.Error(codes.Unauthenticated, `invalid basic auth format`)
+	}
+
+	user, password := cs[:so], cs[so+1:]
+
+	return user, password, nil
+}
+
+func ConstructURL(opaServer, fieldList string) string {
+	strNoSpace := strings.Trim(fieldList, " ")
+	opapath := strings.Replace(strNoSpace, ",", "/", -1)
+	return opaServer + opapath
+}
+
+func HttpRequest(method, url, username, password string) (string,  error) {
+
+	client := &http.Client{
+		//Jar: cookieJar,
+		CheckRedirect: redirectPolicyFunc,
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", errors.Wrap(err, "http.NewRequest in OPA: ")
+	}
+	req.Header.Add("Authorization","Basic " + basicAuth(username,password))
+
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", errors.Wrap(err, "client.Do in OPA: ")
+	}
+	return string(body), nil
+}
+
+func Authen(method, url, username, password string) (bool, error) {
+	response, err := HttpRequest(method, url, username,password)
+	if err != nil {
+		return false, err
+	}
+	if strings.HasPrefix(response, "Success"){
+		return true, nil
+	} else if strings.HasPrefix(response, "Error") {
+		return false, nil
+	} else {
+		return false, errors.Errorf("Unexpected result.")
+	}
+
+}
+
